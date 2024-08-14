1cb6a8493c4d4c2cd298289de41fb63a7d202e0d
Fabian Mautino <fabian_mautino@usmp.pe>
2024-08-14 09:52:59 -0500

    pagina implementada

--- components/WebViewScreen.js
+++ components/WebViewScreen.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, Platform, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function WebViewScreen() {
+  if (Platform.OS === 'web') {
+    return (
+      <View style={styles.container}>
+        <iframe
+          src="https://en.wikipedia.org/wiki/Main_Page"
+          style={styles.webContainer}
+          title="WebView"
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: 'https://example.com' }} />
+      <WebView source={{ uri: 'https://en.wikipedia.org/wiki/Main_Page' }} />
     </View>
   );
 }
@@ -13,5 +25,10 @@ export default function WebViewScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  webContainer: {
+    width: '100%',
+    height: '100%',
+    border: 'none',
   },
 });
